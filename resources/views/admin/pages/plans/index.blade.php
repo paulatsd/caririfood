061f32744736f09aea47205b2e82eb99c4f26251
Paula Tatyene <tatyenedantas@gmail.com>
2021-05-15 23:34:58 -0300

    cria página inicial de planos

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Cariri Food')
+
+@section('content_header')
+    <h1>Planos</h1>
+@stop
+
+@section('content')
+    <p>Listagens dos planos do Cariri Food.</p>
+@stop
